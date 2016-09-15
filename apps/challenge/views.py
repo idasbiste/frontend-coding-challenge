d96eea25e5d93ba37dac48ae516031cf7e106626
Eduardo Fernandes <idasbistefernandes@gmail.com>
2016-09-15 17:31:44 +0100

    Minor modifications on challenge views.py and overall settings

--- apps/challenge/views.py
+++ apps/challenge/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World! You're at the polls index.")
+	template = loader.get_template("template.html")
+	return HttpResponse(template.render(request))
