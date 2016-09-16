20ae3f2e93b028129766b5f55c0f6a67df50a2c8
Eduardo Fernandes <idasbistefernandes@gmail.com>
2016-09-16 13:15:45 +0100

    Add support to spelling errors and its styling

--- web_interface/static/js/app.js
+++ web_interface/static/js/app.js
@@ -1,21 +1,25 @@
 (function () {
 
-	var dataLayer = new DataLayer(),
-		utils = new Utils();
+	var utils = new Utils();
 
 	var text_tpl = _.template($("#text-template").html()),
 		lang_tpl = _.template($("#lang-template").html()),
 		src, trg;
 
-	dataLayer.restGet("/static/mocks/task1.json")
+	utils.getData()
 		.then(function (data) {
 			var lastClickedElement = null,
 				lastHoveredElement = null,
 				wrapperClickedClass = "",
 				wrapperHoveredClass = "";
 				
-			src = utils.getTextSegments(data.source_segments),
-			trg = utils.getTextSegments(data.target_segments);
+			src = utils.getNuggets(data.source_segments),
+			trg = utils.getNuggets(data.target_segments);
+
+			if (src.length != trg.length) 
+			{
+				throw new Error("Different number of source and target segments");
+			}
 
 			// Filling the lanugage template
 			$("#source-lang").html(lang_tpl({ language: data.source_language }));
@@ -23,15 +27,17 @@
 
 
 			// Filling the text template
-			$("#source-text .text-container").html(text_tpl({ segments: src }));
-			$("#target-text .text-container").html(text_tpl({ segments: trg }));
+			$("#source-text .text-container").html(text_tpl({ nuggets: src }));
+			$("#target-text .text-container").html(text_tpl({ nuggets: trg }));
 
 			utils.removeHTMLWhiteSpace($("#source-text .text-container"));
 			utils.removeHTMLWhiteSpace($("#target-text .text-container"));
+			utils.setGlossaryTerms($("#target-text .text-container"), trg);
 
-			$(".text-wrapper", "#target-text").on('click', function () {
+			$(".text-wrapper", "#target-text").on('click', function (e) {
 				if (lastClickedElement && lastClickedElement != this) 
 				{
+					wrapperClickedClass = wrapperClickedClass.replace(" ", ".");
 					$("." + wrapperClickedClass).removeClass("clicked");
 				}
 				
@@ -40,8 +46,10 @@
 				
 				$("." + wrapperHoveredClass).eq(0).removeClass("hovered");
 				$("." + wrapperClickedClass).addClass("clicked");
+
+				e.stopPropagation();
 			})
-			.hover(function () {
+			.hover(function (e) {
 				if (lastHoveredElement && lastHoveredElement != this) 
 				{
 					$("." + wrapperHoveredClass).eq(0).removeClass("hovered");
@@ -54,37 +62,81 @@
 				{
 					$("." + wrapperHoveredClass).eq(0).addClass("hovered");
 				}
+
+				e.stopPropagation();
 			})
-			.mouseleave(function () {
+			.mouseleave(function (e) {
 				if (wrapperHoveredClass)
 				{
 					$("." + wrapperHoveredClass).removeClass("hovered");
-					$("." + wrapperClickedClass).eq(1).removeClass("clicked");
+					if (wrapperClickedClass) 
+					{
+						wrapperClickedClass = wrapperClickedClass.replace(" ", ".");
+						$("." + wrapperClickedClass).eq(1).removeClass("clicked");
+					}
 				}
+
+				e.stopPropagation();
 			});
 
+			$("p.text-wrapper").on('keypress', function () {
+				var self = this,
+					innerHTML = $(this).html();
+				utils.delayInput(
+					function () {
 
-			/*$("#container").on('click', function () {
+						var segment = $(self).html(),
+							glossaries = _.map($(".glossary-term-wrapper", self), function (el) { return $(el).html(); });
+
+						_.each(glossaries, function (glossary) {
+							var innerText = segment.match(/<span.*>(.*?)<\/span>/g)[0];
+							segment = segment.replace(innerText, glossary);
+						});
+
+						utils.smartCheckData({
+							src_lang: data.source_language.shortname,
+							trg_lang: data.target_language.shortname,
+							simpleSource: [$("." + $(self).attr('class').replace(/\s/g, ".")).eq(0).html()],
+							simpleTarget: [segment]
+						})
+						.then(function (data) {
+
+							_.each(data.qa_description, function (issues) {
+								if (!!issues.spelling) {
+									_.each(issues.spelling, function (spellingIssues) {
+
+										$(self).html(innerHTML.replace(spellingIssues.errors, "<span class='word-error-wrapper'>" + spellingIssues.errors + "</span>"))
+										var issuesTpl = _.template($("#error-popup-template").html());
+
+
+									})
+								}
+
+								// Continue if statements for more unknown types
+							});
+						})
+						.fail();
+					}, 
+					1500
+				);
+			});
+
+			$("#container").on('click', function () {
 				if (wrapperClickedClass)
 				{
 					$("." + wrapperClickedClass).removeClass("clicked");
 				}
-			});*/
+			});
 
 			return data;
 
 		})
 		.then(function (getData) {
-			dataLayer.restPost("https://jobcheck.unbabel.com/analyze_job_segments", true, {
-				src_segments: _.map(src, function (obj) {
-					return obj.segment
-				}),
-				trg_segments: _.map(trg, function (obj) {
-					return obj.segment
-				}),
+			utils.smartCheckData({
 				src_lang: getData.source_language.shortname,
 				trg_lang: getData.target_language.shortname,
-				fast_analysis: true
+				source: src,
+				target: trg
 			})
 			.then(function (data) {
 				console.log("POST obtained results");
@@ -92,7 +144,7 @@
 			})
 			.fail(function () {
 				throw new Error("Error on SmartCheck first POST");
-			})
+			});
 		})
 		.fail(function () {
 			throw new Error("Error on annotations GET");
--- web_interface/static/js/utils.js
+++ web_interface/static/js/utils.js
@@ -2,36 +2,116 @@
 
 function Utils() {
 	
-	this.getTextSegments = function (data) {
+	var dataLayer = new DataLayer();
 
-		var segments = [];
+	this.getNuggets = function (data) {
+
+		var nuggets = [];
 
 		_.each(data, function (el) {
 
 			var i = 0,
 				markupTemp = el.annotations.markup,
-				textProperty = !!el.template ? "template" : "text";
+				textProperty = !!el.template ? "template" : "text",
+				complexSeg = {};
+
+			complexSeg.isTarget = !!el.template;
+			complexSeg.id = el.id;
+			complexSeg.text = el.template || el.text;
+			complexSeg.glossary = el.annotations.glossary;
+			complexSeg.segments = [];
 
 			while (i < markupTemp.length) {
-				var complexSeg = {};
-				complexSeg.isTarget = !!el.template;
-				complexSeg.text = el.template || el.text;
-				complexSeg.glossary = el.annotations.glossary;
-				complexSeg.id = el.id + '-' + i;
-				complexSeg.segment = el[textProperty].slice(markupTemp[i++].position, markupTemp[i++].position);
-				segments.push(complexSeg);
+				var segment = {};
+				segment.id = el.id + '-' + i;
+				segment.text = el[textProperty].slice(markupTemp[i++].position, markupTemp[i++].position);
+				complexSeg.segments.push(segment);
 			}
 
+			nuggets.push(complexSeg);
+
+		});
+
+		return nuggets;
+
+	};
+
+
+	// Get text segments
+	this.getTextSegments = function (nuggets) {
+
+		var segments = [];
+
+		_.each(nuggets, function (nug) {
+			_.each(nug.segments, function (seg) {
+				segments.push(seg.text);
+			})
 		});
 
 		return segments;
 
 	};
 
+
+	// Remove unnecessary whitespace from inline span elements
 	this.removeHTMLWhiteSpace = function (element) {
 
 		element.html(element.html().replace(/>\s+</g, "><").trim());
 
 	};
+
+
+	// Set the correct markup for the glossary items - uneditable content
+	this.setGlossaryTerms = function (element, nuggets) {
+
+		_.each(nuggets, function (seg) {
+
+			_.each(seg.glossary, function (glossary) {
+
+				var content = element.html(),
+					term = seg.text.slice(glossary.start, glossary.start + glossary.length);
+
+				element.html(content.replace(term, "<span class='glossary-term-wrapper' contenteditable='false'>" + term + "</span>"));
+
+			})
+
+		})
+
+	};
+
+
+	// Get task data
+	this.getData = function () {
+		return dataLayer.restGet("/static/mocks/task1.json");
+	};
+
+
+	// Get initial text annotations by sendind a POST request
+	this.smartCheckData = function (data) {
+
+		return dataLayer.restPost("https://jobcheck.unbabel.com/analyze_job_segments", true, {
+			src_segments: data.simpleSource || this.getTextSegments(data.source),
+			trg_segments: data.simpleTarget || this.getTextSegments(data.target),
+			src_lang: data.src_lang,
+			trg_lang: data.trg_lang,
+			fast_analysis: true
+		});
+	};
+
+	this.smartCheckIssues = function (data) {
+
+		
+		
+	};
+
+
+	// It delays the keypress event to send a POST request to get text issues
+	this.delayInput = (function () {
+		var timer = 0;
+		return function (callback, ms) {
+			clearTimeout(timer);
+			timer = setTimeout(callback, ms);
+		};
+	})();
 
 }
